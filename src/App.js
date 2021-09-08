7fbaecae894d23a9fe28640f8dd996e25bb331d8
Keyvan Mahmoudi <iamkeyvan72@gmail.com>
2021-09-08 19:59:45 +0430

    Create package

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import classes from './App.module.css';
+
+export default class App extends React.Component {
+  render() {
+    return <div className={classes.container}>Hello World</div>;
+  }
+}
